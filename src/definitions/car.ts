24fc9ec4e9f046e425609b783ee6c325f11d28d7
max-hans <m.hans@me.com>
2023-12-29 21:21:36 +0100

    refactor types

--- src/definitions/car.ts
+++ src/definitions/car.ts
@@ -1,28 +1,27 @@
 import { z } from "zod";
 import { SchemaDefinition } from "../types";
-export const typeDefinition = z.object({
-  brand: z.string().describe("the manufacturer"),
-  model: z.string().describe("the model name of the car"),
-  engine: z.object({
-    type: z.enum(["electric", "gasoline", "diesel", "other"]),
-  }),
-  keywords: z
-    .array(z.string())
-    .describe(
-      "a list of max 10 keywords like 'sportscar', 'sedan', 'offroad' that could be helpful when searching"
-    ),
-  price: z
-    .object({ value: z.number(), unit: z.enum(["usd", "eur"]) })
-    .describe("the price of the car in euros"),
-});
 
-export type TypeDefinition = z.infer<typeof typeDefinition>;
-
-const recipe: SchemaDefinition = {
+const car: SchemaDefinition = {
   ref: 3,
   title: "car",
   description: "A schema to save car models",
-  typeDefinition,
+  typeDefinition: z.object({
+    brand: z.string().describe("the manufacturer"),
+    model: z.string().describe("the model name of the car"),
+    engine: z.object({
+      type: z.enum(["electric", "gasoline", "diesel", "other"]),
+    }),
+    keywords: z
+      .array(z.string())
+      .describe(
+        "a list of max 10 keywords like 'sportscar', 'sedan', 'offroad' that could be helpful when searching"
+      ),
+    price: z
+      .object({ value: z.number(), unit: z.enum(["usd", "eur"]) })
+      .describe("the price of the car in euros"),
+  }),
 };
 
-export default recipe;
+export type TypeDefinition = z.infer<typeof car.typeDefinition>;
+
+export default car;
