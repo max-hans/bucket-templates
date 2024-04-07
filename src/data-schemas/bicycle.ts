e49e6779b5199f0433275569e6e23cd515d6dbd5
max-hans <m.hans@me.com>
2024-04-07 19:26:56 +0200

    add bicycle

---
+++ src/data-schemas/bicycle.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { SchemaDefinition } from "../types";
+import { definitionBase } from "../zod-schemas";
+
+const car: SchemaDefinition = {
+  title: "car",
+  description: "A schema to save bicycles",
+  typeDefinition: definitionBase.extend({
+    brand: z.string().describe("the manufacturer"),
+    model: z.string().describe("the model name of the bicycle"),
+    type: z.object({
+      type: z.enum(["road", "mountainbike", "gravel", "triathlon", "other"]),
+    }),
+    frameMaterial: z.object({
+      type: z.enum(["steel", "aluminium", "carbon", "titanium"]),
+    }),
+
+    price: z
+      .object({ value: z.number(), unit: z.enum(["usd", "eur"]) })
+      .describe("the price of the bicycle in euros or usd"),
+  }),
+};
+
+export type TypeDefinition = z.infer<typeof car.typeDefinition>;
+
+export default car;
